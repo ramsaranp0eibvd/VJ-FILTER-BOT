6d12dfbe02d76f02bf2d229945d31bb664aacfd6
mediashow203 <159476387+mediashow203@users.noreply.github.com>
2024-04-27 11:53:34 -0400

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,8 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 from pyrogram import Client, filters
 import datetime
 import time
@@ -11,10 +6,8 @@ from info import ADMINS
 from utils import broadcast_messages, broadcast_messages_group
 import asyncio
         
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
-async def pm_broadcast(bot, message):
-    if not message.reply_to_message:
-        return await message.reply_text("<b>Reply this command to your broadcast message.</b>")
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
@@ -26,40 +19,28 @@ async def pm_broadcast(bot, message):
     blocked = 0
     deleted = 0
     failed =0
+
     success = 0
     async for user in users:
-        if 'id' in user:
-            pti, sh = await broadcast_messages(int(user['id']), b_msg)
-            if pti:
-                success += 1
-            elif pti == False:
-                if sh == "Blocked":
-                    blocked += 1
-                elif sh == "Deleted":
-                    deleted += 1
-                elif sh == "Error":
-                    failed += 1
-            done += 1
-            if not done % 20:
-                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-        else:
-            # Handle the case where 'id' key is missing in the user dictionary 
-            done += 1
-            failed += 1
-            if not done % 20:
-                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
+            success += 1
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
+                failed += 1
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS))
+@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def broadcast_group(bot, message):
-    if not message.reply_to_message:
-        return await message.reply_text("<b>Reply this command to your broadcast message.</b>")
     groups = await db.get_all_chats()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
